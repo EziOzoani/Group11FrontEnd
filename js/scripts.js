96d5ce2f24e57b30e61c0a28cccf80f479cf2695
ktlait <katelait@icloud.com>
2018-03-25 18:34:21 +0100

    got markers to work on the map

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,3 @@
-// JavaScript source code
-function initMap() {
-    var uluru = { lat: 53.3432, lng: -6.25059 };
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: uluru
-    });
-    var marker = new google.maps.Marker({
-        position: uluru,
-        map: map
-    });
-}
-
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
 output.innerHTML = slider.value;
